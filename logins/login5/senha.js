e0ca164a299795f9e677563d72ac504d9dde5832
Jorge Cunha <matheusaraujocunha.56@gmail.com>
2026-09-21 21:21:53 -0300

    js e valid

--- logins/login5/senha.js
+++ logins/login5/senha.js
@@ -1,1 +1,11 @@
-let senha = 
+let senha = document.getElementById("Senha");
+let icone = document.getElementsByClassName("olho");
+
+function mostrar_senha (){
+    if (senha.type !== "text"){
+        senha.type = "text";
+    }
+    else{
+        senha.type = "password";
+    }
+}
